5fbe9a9d8c0152b1a2dc803e216ed6512528b53c
mpegg28 <michaelpegg1794@gmail.com>
2024-08-22 09:38:44 -0500

    Update mean_type_pegg.py
    Added my name, data, and x,y, and z variable

--- mean_temp.py
+++ mean_temp.py
@@ -3,20 +3,20 @@
 
 """
     File name: mean_temp.py
-    Author: ENTER YOUR  NAME
+    Author: Michael Pegg
     Description:  Calculates mean daily temperature from two user defined variables representing a daily high (x) and low (y) temperature
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT (e.g., 08/23/2023)
+    Date created: ENTER TODAY'S DATE in 08/22/2024 FORMAT (e.g., 08/23/2023)
     Python Version: 3.9.16
 """
 
 # Assign variable x the value for the previous day's high temperature
-x = 
+x = 86
 
 # Assign variable y the value for the previous day's low temperature
-y =  
+y =  61
 
 # Create variable z and use mathematical operators to calculate the mean temperature
-z = 
+z = (x+y)/2
 
 # Use this custom statement that prints the result to screen
 print("Yesterday's mean daily temperature was {0} degrees.".format(z))
