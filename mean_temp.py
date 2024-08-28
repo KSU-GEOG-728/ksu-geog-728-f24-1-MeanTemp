53b32cffc8609f9290fb9f1e68b72fadc7317e74
Iphy16 <ifeomaokonye.france@gmail.com>
2024-08-28 07:53:03 -0500

    Update mean_temp.py
    I included values to calculate the mean temp.

--- mean_temp.py
+++ mean_temp.py
@@ -3,20 +3,21 @@
 
 """
     File name: mean_temp.py
-    Author: ENTER YOUR  NAME
+    Author: Ifeoma Okonye
     Description:  Calculates mean daily temperature from two user defined variables representing a daily high (x) and low (y) temperature
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT (e.g., 08/23/2023)
+    Date created: 08/28/2023
     Python Version: 3.9.16
 """
 
 # Assign variable x the value for the previous day's high temperature
-x = 
+x = 35
 
 # Assign variable y the value for the previous day's low temperature
-y =  
+y =  21
 
 # Create variable z and use mathematical operators to calculate the mean temperature
-z = 
+z = (x + y)/2
 
 # Use this custom statement that prints the result to screen
 print("Yesterday's mean daily temperature was {0} degrees.".format(z))
+
